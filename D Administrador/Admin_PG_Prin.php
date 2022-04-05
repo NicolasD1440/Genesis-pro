1c46bc34f832720faad6e058e129101a2260fad8
Purenega95 <cristian.cruz-N@uniminuto.edu.co>
2022-04-05 18:56:53 -0500

    Cambios

---
+++ D Administrador/Admin_PG_Prin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina principal</title>
+    <link rel="stylesheet" href="css/Estilo.css">
+    <link rel="stylesheet" type="text/css" href="../A CSS NAV/Nav.css">
+  </head>
+  <body>
+    <ul class="Menu">
+      <li><a href="../C Pagina principal/Index.html">Inicio</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+    </ul>
+    <h1>Administracion</h1>
+    <div class="container">
+      <div class="main">
+        <img src="../B Recursos compartidos/Usuario.png" alt="Imagen del profe">
+        <h3>Hola "NOMBRE DEL ADMIN" </h3>
+      </div>
+      <p>Esta es la página de entrada para el servicio de administracion de Génesis. Desde aquí, puede gestionar los servicios disponibles por la institucion:</p>
+      <ul>
+        <li class="li"><a href="">Actualizar contraseña</a></li>
+        <li class="li"><a href="">Actualizar datos personales</a></li>
+        <li class="li"><a href="">Registrar usuarios</a></li>
+        <li class="li"><a href="">Eliminar usuarios</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
---
+++ E Profesor/Prof_PG_Prin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina principal</title>
+    <link rel="stylesheet" href="css/Estilo.css">
+    <link rel="stylesheet" type="text/css" href="../A CSS NAV/Nav.css">
+  </head>
+  <body>
+    <ul class="Menu">
+      <li><a href="../C Pagina principal/Index.html">Inicio</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+    </ul>
+    <h1>Servicios para docentes</h1>
+    <div class="container">
+      <div class="main">
+        <img src="../B Recursos compartidos/Usuario.png" alt="Imagen del profe">
+        <h3>Hola "NOMBRE DEL PROFE" </h3>
+      </div>
+      <p>Esta es la página de entrada para el autoservicio Génesis Profesores. Desde aquí, puede gestionar los servicios disponibles por la institucion:</p>
+      <ul>
+        <li class="li"><a href="">Actualizar contraseña</a></li>
+        <li class="li"><a href="">Actualizar datos personales</a></li>
+        <li class="li"><a href="">Ingresar notas</a></li>
+        <li class="li"><a href="">Perfin del docente</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
