323b6800514ba0d66045433f23554f110ff7c423
Purenega95 <cristian.cruz-N@uniminuto.edu.co>
2022-04-13 15:51:47 -0500

    Nuvas interfaces ahora siuuuuu

---
+++ E Profesor/Agregar_Notas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Agregar notas</title>
+    <link rel="stylesheet" href="css/Estilo_ADD_Notas.css">
+    <link rel="stylesheet" type="text/css" href="../A CSS NAV/Nav.css">
+    <script src="https://kit.fontawesome.com/f959a384d4.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <ul class="Menu">
+      <li><a href="Prof_PG_Prin.php"><i class="fas fa-arrow-left"></i> Volver</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+      <li><a href="#">Por definir</a></li>
+    </ul>
+    <main class="Opciones">
+      <select class="NRCS" name="">
+        <option hidden selected value="1">Seleccione un NRC</option>
+        <option value="valie1">5580</option>
+      </select>
+    </main>
+    <div class="Lista">
+
+    </div>
+  </body>
+</html>
