7ea8b87680a3867c7caa34206c3575e82412402b
NicolasD1440 <nicolascastiblanco6@gmail.com>
2022-05-16 21:43:29 -0500

    gato

--- B Recursos compartidos/Alertas/Bienvenido.js
+++ B Recursos compartidos/Alertas/Bienvenido.js
@@ -7,7 +7,7 @@ Swal.fire({
   background: '#fff url(/images/trees.png)',
   backdrop: `
     rgba(0,0,123,0.4)
-    url("/images/nyan-cat.gif")
+    url("https://c.tenor.com/hRtQfhh1Aw4AAAAi/gatito-cansado.gif")
     left top
     no-repeat
   `
